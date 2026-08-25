9b616153724e9723d8f556d941b916e70b30d7ab
amiyo-bhowmik <amiyobhowmik123@gmail.com>
2026-08-25 22:49:59 +0600

    fix: add missing import

--- chhaya-backend/app/main.py
+++ chhaya-backend/app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.responses import RedirectResponse
 from app.api.v1.api import api_router
 from app.core.config import settings
 from app.core.database import pool
